81cef5009738e92fbd4fa9961f6d9d17cfe7068a
Mohamed Hassan ALI <11545656+Mido-o@users.noreply.github.com>
2021-04-24 16:33:56 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,46 +1,71 @@
 const collapsibles = Array.from(document.getElementsByClassName("collapsible"));
-const content = Array.from(document.getElementsByClassName("content"));
+const content = document.getElementsByClassName("content");
 const coll_cont = new Map();
 collapsibles.forEach((value, idx) => coll_cont.set(value, content[idx]));
 let src1 = "images/icon-hamburger.svg";
 let src2 = "images/icon-close.svg";
 
-let temp, cont;
+let temp, contTemp, cont, img;
+let test, testTemp;
 
 window.addEventListener("click", function (event) {
     let This = event.target;
-    if (This.classList.contains("collapsible")) {
-        console.log("The target is: ", This);
-        temp = This;
-        cont = coll_cont.get(temp);
-        collapse(This);
-    } else if (This !== temp && typeof (cont) !== "undefined") {
+    test = This;
+    /*if ((This !== temp && typeof (temp) !== "undefined")) { //This !== temp && typeof (temp) !== "undefined" && typeof (This.closest(".content")) === "undefined"
         console.log("The other element is: ", This);
         if (cont.style.maxHeight) {
-            temp.style.fontWeight = "normal";
-            temp.style.setProperty("--pseudo-transform", 'rotate(0)');
-            cont.style.maxHeight = null;
+            collapse(temp, cont);
+        }
+    }*/
+    if (This.matches(".collapsible")) {
+        console.log("The target is: ", This);
+        if (This.getAttribute("src")) { img = This; }
+        if (This !== temp && temp !== undefined) {
+            console.log("In the second case");
+            cont = coll_cont.get(This);
+            collapse(This, cont, img);
+        } else {
+            temp = This; //The first time temp is assigned
+            console.log("Temp is set");
+            testTemp = temp;
+            contTemp = coll_cont.get(temp);
+            collapse(temp, contTemp, img);
+        }
+    } else {
+        if (temp !== undefined && contTemp.style.maxHeight) {
+            close(temp, contTemp, img);
         }
     }
 });
 
-function collapse(This) {
-    let content = coll_cont.get(This);
+function collapse(This, content, img) {
     This.classList.toggle("active");
-    let img = This.firstElementChild;
     if (content.style.maxHeight) {
-        This.style.fontWeight = "normal";
-        This.style.setProperty("--pseudo-transform", 'rotate(0)');
-        if (img) {
-            img.src = src1;
-        }
-        content.style.maxHeight = null;
+        close(This, content, img);
     } else {
-        This.style.fontWeight = "bold";
-        This.style.setProperty("--pseudo-transform", 'rotate(-180deg)');
-        if (img) {
-            img.src = src2;
-        }
-        content.style.maxHeight = 500 + "px";
+        open(This, content, img);
     }
+    temp = This;
+    contTemp = content;
+}
+
+function open(This, content, img) {
+    if (temp !== undefined && contTemp.style.maxHeight) {
+        close(temp, contTemp, img);
+    }
+    This.style.fontWeight = "bold";
+    This.style.setProperty("--pseudo-transform", 'rotate(-180deg)');
+    if (img) {
+        img.setAttribute("src", src2);
+    }
+    content.style.maxHeight = 500 + "px";
+}
+
+function close(This, content, img) {
+    This.style.fontWeight = "normal";
+    This.style.setProperty("--pseudo-transform", 'rotate(0)');
+    if (img) {
+        img.setAttribute("src", src1);
+    }
+    content.style.maxHeight = null;
 }
