df67a941129106ca0c60e9aa13cc37f17d7a58c2
Mohamed Hassan ALI <11545656+Mido-o@users.noreply.github.com>
2021-04-25 04:15:17 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,35 +6,27 @@ let src1 = "images/icon-hamburger.svg";
 let src2 = "images/icon-close.svg";
 
 let temp, contTemp, contParent, cont, img, parentImg; //temp: will be the previously collapsible element and contTemp is its content. contParent: the parent content, should have the calss content. cont: is the content of the currently clicked element. img: is the image of the clicked element, if exsits.
-let test, testTemp;
 
 window.addEventListener("click", function (event) {
     let This = event.target; //to retrive the element that was clicked.
-    test = This;
     if (This.matches(".collapsible")) { //if that element has the class collapsible (collapsible element).
-        console.log("The target is: ", This);
         if (This.getAttribute("src")) {
             img = This;
             parentImg = img;
         } else { img = undefined; } //if that element has a photo, retrive it.
         contParent = This.closest(".content"); //retrive the nearest parent, of the clicked element, with the calss content (parent content).
         if (temp !== undefined && This !== temp) { //if the currently clicked element is not the previously clicked element.
-            console.log("2nd case");
             cont = coll_cont.get(This);
             collapse(This, cont, img);
         } else { //the first time a collapsible element is clicked, this condition will be triggered
             temp = This; //The first time temp is assigned
-            console.log("Temp is set");
-            testTemp = temp;
             contTemp = coll_cont.get(temp);
             collapse(temp, contTemp, img);
         }
     } else {
         if (temp !== undefined && contTemp.style.maxHeight) { //if the clicked element is not collapsible and a previous content was open.
-            console.log("3rd case.1");
             close(temp, contTemp, img);
             if (contParent !== null && contParent.style.maxHeight) { //if the parent content is open.
-                console.log("3rd case.2");
                 close(This, contParent, parentImg);
             }
         }
@@ -54,11 +46,9 @@ function collapse(This, content, img) {
 
 function open(This, content, img) {
     if (contTemp !== undefined && contTemp === contParent) { //before opening the new element, and we're inside the content, and if the previously opened content is the parent content, dont't close it. Just update the previous element and its content to be the currently clicked element.
-        console.log("1st open case");
         temp = This;
         contTemp = contTemp = coll_cont.get(temp);
     } else if ((contTemp !== undefined && contTemp !== contParent && contTemp.style.maxHeight)) { //before opening a new element, and we're inside the content, and if the previously opened content is not the parent content, close the previously opened content.
-        console.log("2nd open case");
         close(temp, contTemp, img);
     }
     This.style.fontWeight = "bold";
