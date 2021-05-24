8cb21a9a66033b20df250b7398c5ea058111902d
Mohamed Hassan ALI <11545656+Mido-o@users.noreply.github.com>
2021-05-25 01:23:33 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,68 +2,45 @@ const collapsibles = Array.from(document.getElementsByClassName("collapsible"));
 const content = document.getElementsByClassName("content");
 const coll_cont = new Map();
 collapsibles.forEach((value, idx) => coll_cont.set(value, content[idx]));
-let src1 = "images/icon-hamburger.svg";
-let src2 = "images/icon-close.svg";
+let src1 = "https://raw.githubusercontent.com/Mido-o/blogr-landing-page-main/16f6de776de3e8e0d69f0b3acb005423af6c1c5d/images/icon-hamburger.svg";
+let src2 = "https://raw.githubusercontent.com/Mido-o/blogr-landing-page-main/16f6de776de3e8e0d69f0b3acb005423af6c1c5d/images/icon-close.svg";
+let key, cont, previousKey, previousCont, parent;
 
-let temp, contTemp, contParent, cont, img, parentImg; //temp: will be the previously collapsible element and contTemp is its content. contParent: the parent content, should have the calss content. cont: is the content of the currently clicked element. img: is the image of the clicked element, if exsits.
-
-window.addEventListener("click", function (event) {
-    let This = event.target; //to retrive the element that was clicked.
-    if (This.matches(".collapsible")) { //if that element has the class collapsible (collapsible element).
-        if (This.getAttribute("src")) {
-            img = This;
-            parentImg = img;
-        } else { img = undefined; } //if that element has a photo, retrive it.
-        contParent = This.closest(".content"); //retrive the nearest parent, of the clicked element, with the calss content (parent content).
-        if (temp !== undefined && This !== temp) { //if the currently clicked element is not the previously clicked element.
-            cont = coll_cont.get(This);
-            collapse(This, cont, img);
-        } else { //the first time a collapsible element is clicked, this condition will be triggered
-            temp = This; //The first time temp is assigned
-            contTemp = coll_cont.get(temp);
-            collapse(temp, contTemp, img);
+window.addEventListener("click", function (e) {
+    if (e.target.matches(".collapsible")) { //if the clicked element is collapsible
+        key = e.target;
+        cont = coll_cont.get(key);
+        if (parent && previousCont !== parent && cont !== previousCont) { //for the mobile menu (some collapsible elements will be inside their parent content)
+            previousCont.classList.remove("active");
+            previousKey.style.fontWeight = "normal";
+            previousKey.style.setProperty("--pseudo-transform", 'rotate(0)');
         }
-    } else {
-        if (temp !== undefined && contTemp.style.maxHeight) { //if the clicked element is not collapsible and a previous content was open.
-            close(temp, contTemp, img);
-            if (contParent !== null && contParent.style.maxHeight) { //if the parent content is open.
-                close(This, contParent, parentImg);
+        parent = key.closest(".content"); //retrive the parent content
+        cont.classList.toggle("active");
+        key.style.fontWeight = "bold";
+        key.style.setProperty("--pseudo-transform", 'rotate(-180deg)');
+        if (cont.classList.contains("active")) {
+            if (key.closest("#burger-king")) {
+                key.src = src2;
             }
+        } else {
+            previousKey.style.fontWeight = "normal";
+            key.style.setProperty("--pseudo-transform", 'rotate(0)');
+            if (key.closest("#burger-king")) {
+                key.src = src1;
+            }
+        }
+        previousKey = e.target;
+        previousCont = coll_cont.get(previousKey);
+    } else {
+        const actives = document.getElementsByClassName("active");
+        coll_cont.keys().next().value.src = src1; //retrive a key from map by value (the #burger-king element)
+        for (let i = actives.length - 1; i >= 0; --i) {
+            actives[i].classList.remove("active");
+        }
+        if (previousKey) {
+            previousKey.style.fontWeight = "normal";
+            previousKey.style.setProperty("--pseudo-transform", 'rotate(0)');
         }
     }
 });
-
-function collapse(This, content, img) {
-    content.classList.toggle("active");
-    if (content.style.maxHeight) {
-        close(This, content, img);
-    } else {
-        open(This, content, img);
-    }
-    temp = This;
-    contTemp = content;
-}
-
-function open(This, content, img) {
-    if (contTemp !== undefined && contTemp === contParent) { //before opening the new element, and we're inside the content, and if the previously opened content is the parent content, dont't close it. Just update the previous element and its content to be the currently clicked element.
-        temp = This;
-        contTemp = contTemp = coll_cont.get(temp);
-    } else if ((contTemp !== undefined && contTemp !== contParent && contTemp.style.maxHeight)) { //before opening a new element, and we're inside the content, and if the previously opened content is not the parent content, close the previously opened content.
-        close(temp, contTemp, img);
-    }
-    This.style.fontWeight = "bold";
-    This.style.setProperty("--pseudo-transform", 'rotate(-180deg)');
-    if (img) {
-        img.setAttribute("src", src2);
-    }
-    content.style.maxHeight = 500 + "px";
-}
-
-function close(This, content, img) {
-    This.style.fontWeight = "normal";
-    This.style.setProperty("--pseudo-transform", 'rotate(0)');
-    if (img) {
-        img.setAttribute("src", src1);
-    }
-    content.style.maxHeight = null;
-}
